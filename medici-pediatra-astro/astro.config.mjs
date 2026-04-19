32f015111ae870e4948cc7d2bf11a1786ed7a57b
nickmaukan <nickmaukan@users.noreply.github.com>
2026-04-18 23:08:58 -0500

    Nueva landing: Pediatra Dra. Carolinaidez (07-pediatra.html) - Landify showcase 7 templates

---
+++ medici-pediatra-astro/astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [tailwind()],
+  site: 'https://dra-carolinaidez.com',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
+  }
+});
